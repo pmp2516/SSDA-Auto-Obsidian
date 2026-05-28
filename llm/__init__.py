f38fc74bd4598b491624bbc9517605fb2d347439
Tim Hallyburton <tim.hallyburton@tu-dortmund.de>
2026-05-28 17:59:06 +0200

    fix: add missing export

--- llm/__init__.py
+++ llm/__init__.py
@@ -1,4 +1,5 @@
 from .client import LLMClient
+from .client import VLLMClient
 from .config import LLMConfig
 
-__all__ = ["LLMClient", "LLMConfig"]
+__all__ = ["LLMClient", "VLLMClient", "LLMConfig"]
